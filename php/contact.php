d6b9417ef7175b01aed23d82d7ab1b340d450901
malogarzo <mariano.logarzo@gmail.com>
2018-07-18 20:34:28 -0300

    fix img routes, html typo & logo

---
+++ php/contact.php
@@ -0,0 +1,17 @@
+<?php
+	$to = "info@nexo.site";
+
+	$name = $_POST['name'];
+	$email = $_POST['email'];
+	$message = $_POST['message'];
+
+	$subject = 'Mensaje de chevere.site';
+	$from = 'From: ' . $name;
+	$body = "From: $name \nE-Mail: $email \nMessage: \n$message";
+
+	if (mail ($to, $subject, $body, $from)) {
+		echo '<p>Your message has been sent!</p>';
+	} else {
+		echo '<p class="error">Something went wrong, try again!</p>';
+	}
+?>
